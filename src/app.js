534619ff7f510ea94445d628e1ea48322bece1d2
Matheus Henrique <matheushmms@hotmail.com>
2018-09-11 13:11:16 -0300

    Criando rotas para produtos e acesso ao banco de dados

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const app = express();
 const router = express.Router();
+
+// Conecta ao banco
+mongoose.connect('mongodb://mhst3ch:mhst3ch@ds018258.mlab.com:18258/ndestr');
+
+// Carrega os Models
+
+const Product = require('./models/product');
 
 // Carrega rotas
 const indexRoute = require('./routes/index-route');
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,24 @@
-'use strict';
+'use strict'
+
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    Product.find({active: true}, 'title price slug').then(data=>{
+        res.status(200).send(data);
+    }).catch(e=>{
+        res.status(400).send(e);
+    });
+}
+
 
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product.save().then(x=>{
+        res.status(201).send({ message: "Produto cadastrado com sucesso!"});
+    }).catch(e=>{
+        res.status(400).send({ message: "Falha ao cadastrar o produto", data: e});
+    });
 };
 
 exports.put = (req, res, next) => {
